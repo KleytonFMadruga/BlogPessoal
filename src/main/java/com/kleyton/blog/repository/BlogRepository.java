c83d59f2178a8b169cd627dc1e54e17bbf4af59b
KleytonFMadruga <kleyton.fernandes@dcx.ufpb.br>
2021-02-05 21:12:46 -0300

    integração com o banco de dados postgresql e criação da classe post

---
+++ src/main/java/com/kleyton/blog/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.kleyton.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kleyton.blog.model.Post;
+
+public interface BlogRepository extends JpaRepository<Post, Long>{
+
+}
